829604cfe1bf1d0602e157566e12d3cd9c4dbf32
luiscyriaco <luiscyriaco@hotmail.com>
2025-06-07 20:24:34 -0300

    Primeiro Commit

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,11 @@
+# Criando e atribuindo valores as variáveis A e B
+a = 5
+b = 2
+# Imprimindo o resultado das operações aritmética de Python
+print("Adição............", (a + b))
+print("Subtração.........", (a - b))
+print("Multiplicação.....", (a * b))
+print("Divisão Inteira...", (a // b))
+print("Adição Decimal....", (a / b))
+print("Exponenciação.....", (a ** b))
+print("Resto Divisão.....", (a % b))
