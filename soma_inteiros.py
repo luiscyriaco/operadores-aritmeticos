ff1952ab0a135b4322c3eac85896ca2cf2378737
luiscyriaco <luiscyriaco@hotmail.com>
2025-06-09 00:59:21 -0300

    soma inteiros

---
+++ soma_inteiros.py
@@ -0,0 +1,8 @@
+# Criando variáveis "a" e "b" e solicitando valor ao usuário
+print("Soma de dois números inteiros")
+a = int(input("Primeito Valor...."))
+b = int(input("Segundo Valor....."))
+# Realizando a soma dos valores de "a" e "b"
+soma = a + b
+# Apresentando o resultado da soma ao usuário
+print("Soma.............", soma)
